bbd5d67cf04e79977c6d54da0963b1d6d7efc767
Khanh Phan <khanh.phan.zc@bp.renesas.com>
2026-01-12 10:33:46 +0700

    scripts: Add scripts to build the flash writer and generate a DDR parameter binary file for RZ/G2L
    This commit will add scripts to build the flash writer and generate a DDR parameter binary file for RZ/G2L.
    
    Signed-off-by: Khanh Phan <khanh.phan.zc@bp.renesas.com>

---
+++ scripts/build_flash_writer_g2l.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Sample script to build the flash writer for RZ/G2L.
+#
+
+export ARCH=arm64
+export CROSS_COMPILE=`pwd`/../../gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
+export CC=${CROSS_COMPILE}gcc
+export AS=${CROSS_COMPILE}as
+export LD=${CROSS_COMPILE}ld
+export AR=${CROSS_COMPILE}ar
+export OBJDUMP=${CROSS_COMPILE}objdump
+export OBJCOPY=${CROSS_COMPILE}objcopy
+
+cd ../
+make clean
+make EMMC=ENABLE SERIAL_FLASH=DISABLE BOARD=RZG2L_SMARC_PMIC
