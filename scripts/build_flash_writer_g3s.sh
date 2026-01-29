5e7a074a1699c19ee08758a4fad8d26c633a2a1c
Khanh Phan <khanh.phan.zc@bp.renesas.com>
2026-01-29 13:39:58 +0700

    scripts: build_flash_writer_g3s: Update script to support building for multiple boards
    This commit updates the build script to support multiple boards by passing BOARD as a parameter
    
    Signed-off-by: Khanh Phan <khanh.phan.zc@bp.renesas.com>

--- scripts/build_flash_writer_g3s.sh
+++ scripts/build_flash_writer_g3s.sh
@@ -3,6 +3,8 @@
 # Sample script to build the flash writer for RZ/G3S.
 #
 
+set -ue
+
 export ARCH=arm64
 export CROSS_COMPILE=`pwd`/../../gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
 export CC=${CROSS_COMPILE}gcc
@@ -14,4 +16,4 @@ export OBJCOPY=${CROSS_COMPILE}objcopy
 
 cd ../
 make -f makefile-g3s.gcc-arm clean
-make -f makefile-g3s.gcc-arm BOARD=RZG3S_SMARC
+make -f makefile-g3s.gcc-arm BOARD=${1}
