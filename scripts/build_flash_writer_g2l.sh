7c2389ecc504f79c71181cbb12c1e45ec6f20b78
Khanh Phan <khanh.phan.zc@bp.renesas.com>
2026-01-29 14:08:22 +0700

    scripts: build_flash_writer_g2l: Update script to support building for multiple boards
    This commit updates the build script to support multiple boards by passing BOARD as a parameter.
    
    Signed-off-by: Khanh Phan <khanh.phan.zc@bp.renesas.com>

--- scripts/build_flash_writer_g2l.sh
+++ scripts/build_flash_writer_g2l.sh
@@ -3,6 +3,8 @@
 # Sample script to build the flash writer for RZ/G2L.
 #
 
+set -ue
+
 export ARCH=arm64
 export CROSS_COMPILE=`pwd`/../../gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
 export CC=${CROSS_COMPILE}gcc
@@ -14,4 +16,4 @@ export OBJCOPY=${CROSS_COMPILE}objcopy
 
 cd ../
 make clean
-make EMMC=ENABLE SERIAL_FLASH=DISABLE BOARD=RZG2L_SMARC_PMIC
+make EMMC=ENABLE SERIAL_FLASH=DISABLE BOARD=${1}
