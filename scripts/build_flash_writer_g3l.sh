c2f5c41345e95dca3651c116c24ff6e15a843cea
Khanh Phan <khanh.phan.zc@bp.renesas.com>
2026-04-24 14:49:33 +0700

    scripts: Add scripts to build the flash writer and generate a DDR parameter binary file for RZ/G3L
    This commit will add scripts to build the flash writer and generate a DDR parameter binary file for RZ/G3L.
    
    Signed-off-by: Khanh Phan <khanh.phan.zc@bp.renesas.com>

---
+++ scripts/build_flash_writer_g3l.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Sample script to build the flash writer for RZ/G3L.
+#
+
+set -ue
+
+export ARCH=arm64
+export CROSS_COMPILE=`pwd`/../../gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
+export CC=${CROSS_COMPILE}gcc
+export AS=${CROSS_COMPILE}as
+export LD=${CROSS_COMPILE}ld
+export AR=${CROSS_COMPILE}ar
+export OBJDUMP=${CROSS_COMPILE}objdump
+export OBJCOPY=${CROSS_COMPILE}objcopy
+
+cd ../
+make -f makefile-g3l.gcc-arm clean
+make -f makefile-g3l.gcc-arm BOARD=${1} DDR_PARAM_LOAD=ENABLE DDR_DEBUG=1
